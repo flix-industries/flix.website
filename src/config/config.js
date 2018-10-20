ee28c81eead505271759cae624f256c841431608
VK <vitaliy.krynytskyy@gmail.com>
2018-10-20 18:19:09 +0100

    Updating to test travis

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,10 @@
-import functions from 'firebase-functions';
-
 const config = {
-    apiKey: functions.config().todoservice.apikey,
-    authDomain: functions.config().todoservice.authdomain,
-    databaseURL: functions.config().todoservice.databaseurl,
-    projectId: functions.config().todoservice.projectid,
-    storageBucket: functions.config().todoservice.storagebucket,
-    messagingSenderId: functions.config().todoservice.messagingsenderid,
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId
 };
 
 export default config;
