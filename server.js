bef432caa5f6b67768cd956d772693535abd7bd3
VK <vitaliy.krynytskyy@gmail.com>
2018-10-20 09:45:25 +0100

    updating prod server config

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
+const PORT = process.env.PORT || 1337;
+
+app.use(cors()); // enable cors
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(8080);
+app.listen(PORT, () => {
+    console.log(`Live on port ${PORT}`);
+});
